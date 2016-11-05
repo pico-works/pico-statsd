75e263c2cdc275e2c431074b59a2f7d875d88912
John Ky <newhoggy@gmail.com>
2016-11-05 14:43:39 +1100

    Initial project

---
+++ project/Build.scala
@@ -0,0 +1,43 @@
+import sbt.Keys._
+import sbt._
+
+object Build extends sbt.Build {  
+  val pico_atomic     = "org.pico"              %%  "pico-atomic"           % "0.2.1"
+  val pico_disposal   = "org.pico"              %%  "pico-disposal"         % "1.0.8"
+  val pico_event      = "org.pico"              %%  "pico-event"            % "5.0.0"
+  
+  val statsd_client   = "com.datadoghq"         %   "java-dogstatsd-client" % "2.2"
+
+  val specs2_core     = "org.specs2"            %%  "specs2-core"           % "3.8.6"
+
+  implicit class ProjectOps(self: Project) {
+    def standard(theDescription: String) = {
+      self
+          .settings(scalacOptions in Test ++= Seq("-Yrangepos"))
+          .settings(publishTo := Some("Releases" at "s3://dl.john-ky.io/maven/releases"))
+          .settings(description := theDescription)
+          .settings(isSnapshot := true)
+          .settings(resolvers += Resolver.sonatypeRepo("releases"))
+          .settings(addCompilerPlugin("org.spire-math" % "kind-projector" % "0.9.3" cross CrossVersion.binary))
+    }
+
+    def notPublished = self.settings(publish := {}).settings(publishArtifact := false)
+
+    def libs(modules: ModuleID*) = self.settings(libraryDependencies ++= modules)
+
+    def testLibs(modules: ModuleID*) = self.libs(modules.map(_ % "test"): _*)
+  }
+
+  lazy val `pico-fake` = Project(id = "pico-fake", base = file("pico-fake"))
+      .standard("Fake project").notPublished
+      .testLibs(specs2_core)
+
+  lazy val `pico-statsd` = Project(id = "pico-statsd", base = file("pico-statsd"))
+      .standard("Statsd library")
+      .libs(pico_atomic, pico_disposal, pico_event, statsd_client)
+      .testLibs(specs2_core)
+
+  lazy val all = Project(id = "pico-event-project", base = file("."))
+      .notPublished
+      .aggregate(`pico-statsd`, `pico-fake`)
+}
