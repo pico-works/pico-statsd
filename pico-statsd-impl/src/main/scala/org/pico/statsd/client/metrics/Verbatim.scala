254056397eeccde4517eeb8ea06fd194412a39fe
John Ky <newhoggy@gmail.com>
2016-11-22 09:40:28 +1100

    Fix typo

--- pico-statsd-impl/src/main/scala/org/pico/statsd/client/metrics/Verbatim.scala
+++ pico-statsd-impl/src/main/scala/org/pico/statsd/client/metrics/Verbatim.scala
@@ -5,7 +5,7 @@ import org.pico.statsd.client.Metric
 case class Verbatim(text: String) extends AnyVal
 
 object Verbatim {
-  implicit val verbatime_Metric_3qR2szH = new Metric[Verbatim] {
+  implicit val verbatim_Metric_3qR2szH = new Metric[Verbatim] {
     override def encodeMetric(metric: Verbatim, prefix: String, tagger: Seq[String] => String): String = {
       metric.text
     }
