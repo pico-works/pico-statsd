bdf0e8bce5ca32ebd2262e221f6f83e951d26fdf
John Ky <newhoggy@gmail.com>
2016-11-29 11:28:03 +1100

    Better API

--- pico-statsd/src/main/scala/org/pico/statsd/syntax/event/package.scala
+++ pico-statsd/src/main/scala/org/pico/statsd/syntax/event/package.scala
@@ -3,7 +3,7 @@ package org.pico.statsd.syntax
 import org.pico.disposal.std.autoCloseable._
 import org.pico.event.{Bus, SinkSource, Source}
 import org.pico.statsd._
-import org.pico.statsd.datapoint._
+import org.pico.statsd.datapoint.{IntegralHistogramMetric, _}
 
 import scala.concurrent.duration.Deadline
 
@@ -50,7 +50,6 @@ package object event {
   
   //-------------------- Timers -------------------------------------------
   implicit class SinkSourceOps_Timer_Rht98nT[A, B](val self: SinkSource[A, B]) extends AnyVal {
-    
     @inline
     def withSimpleTimer(metric: String, sampleRate: SampleRate, tags: String*)(implicit c: StatsdClient): SinkSource[A, B] = {
       val configuredClient = c.sampledAt(sampleRate)
@@ -71,7 +70,6 @@ package object event {
   
   
   implicit class SourceOps_Timer_Rht98nT[A](val self: Source[A]) extends AnyVal {
-    
     @inline
     def withSimpleTimer(metric: String, sampleRate: SampleRate, tags: String*)(implicit c: StatsdClient): Source[A] = {
       val configuredClient = c.sampledAt(sampleRate)
@@ -91,7 +89,6 @@ package object event {
   
   //-------------------- COUNTERS -------------------------------------------
   implicit class SinkSourceOps_Counter_Rht98nT[A, B](val self: SinkSource[A, B]) extends AnyVal {
-   
     @inline
     def withCounter(metric: String, delta: Long, sampleRate: SampleRate, tags: String*)
                    (implicit c: StatsdClient): SinkSource[A, B] = {
@@ -112,7 +109,6 @@ package object event {
   }
   
   implicit class SourceOps_Counter_Rht98nT[A](val self: Source[A]) extends AnyVal {
-
     @inline
     def withCounter(metric: String, delta: Long, sampleRate: SampleRate, tags: String*)
                    (implicit c: StatsdClient): Source[A] = {
@@ -134,7 +130,6 @@ package object event {
   
   //-------------------- GAUGES --------------------------------------------
   implicit class SinkSourceOps_Gauge_Rht98nT[A, B](val self: SinkSource[A, B]) extends AnyVal {
-  
     @inline
     def withIntegralGauge(metric: String, value: B => Long, sampleRate: SampleRate, tags: String*)
                          (implicit c: StatsdClient): SinkSource[A, B] = {
@@ -155,7 +150,6 @@ package object event {
   }
   
   implicit class SourceOps_Gauge_Rht98nT[A](val self: Source[A]) extends AnyVal {
-  
     @inline
     def withIntegralGauge(metric: String, value: A => Long, sampleRate: SampleRate, tags: String*)
                            (implicit c: StatsdClient): Source[A] = {
@@ -164,7 +158,7 @@ package object event {
       self += self.effect(a => configuredClient.sample(LongGauge(value(a)))(sampler))
       self
     }
-    
+
     @inline
     def withFractionalGauge(metric: String, value: A => Double, sampleRate: SampleRate, tags: String*)
                  (implicit c: StatsdClient): Source[A] = {
@@ -177,7 +171,6 @@ package object event {
   
   //-------------------- HISTOGRAMS ------------------------------------------
   implicit class SinkSourceOps_Histogram_Rht98nT[A, B](val self: SinkSource[A, B]) extends AnyVal {
-  
     @inline
     def withIntegralHistogram(metric: String, value: B => Long, sampleRate: SampleRate, tags: String*)
                          (implicit c: StatsdClient): SinkSource[A, B] = {
@@ -198,7 +191,6 @@ package object event {
   }
   
   implicit class SourceOps_Histogram_Rht98nT[A](val self: Source[A]) extends AnyVal {
-  
     @inline
     def withIntegralHistogram(metric: String, value: A => Long, sampleRate: SampleRate, tags: String*)
                              (implicit c: StatsdClient): Source[A] = {
@@ -214,6 +206,14 @@ package object event {
       val configuredClient = c.sampledAt(sampleRate)
       val sampler = Metric[DoubleHistogram](FractionalHistogramMetric(metric), TaggedWith(tags.toList))
       self += self.effect(a => configuredClient.sample(DoubleHistogram(value(a)))(sampler))
+      self
+    }
+  }
+
+  implicit class SourceOps_MetricThings_Rht98nT[A](val self: Source[A]) extends AnyVal {
+    @inline
+    def withMetric(metric: Metric[A])(implicit c: StatsdClient): Source[A] = {
+      self += self.effect(a => c.sample(a)(metric))
       self
     }
   }
