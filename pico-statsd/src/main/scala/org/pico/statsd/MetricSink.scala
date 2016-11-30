da08445b3ce03e80f8522ca3a7f059c29bfbf772
John Ky <newhoggy@gmail.com>
2016-11-30 11:08:52 +1100

    More MetricSink constructors

--- pico-statsd/src/main/scala/org/pico/statsd/MetricSink.scala
+++ pico-statsd/src/main/scala/org/pico/statsd/MetricSink.scala
@@ -9,4 +9,32 @@ object MetricSink {
       statsdClient.sample(a)(metric)
     }
   }
+
+  def apply[A](aspect: String, metric: Metric[A])(implicit statsdClient: StatsdClient): Sink[A] = {
+    val configuredClient = statsdClient.withAspect(aspect)
+
+    Sink[A] { a =>
+      configuredClient.sample(a)(metric)
+    }
+  }
+
+  def apply[A](aspect: String, metric: Metric[A], sampleRate: SampleRate)(implicit statsdClient: StatsdClient): Sink[A] = {
+    val configuredClient = statsdClient.withAspect(aspect).sampledAt(sampleRate)
+
+    Sink[A] { a =>
+      configuredClient.sample(a)(metric)
+    }
+  }
+
+  def apply[A](implicit statsdClient: StatsdClient, metric: Metric[A]): Sink[A] = {
+    MetricSink(metric)(statsdClient)
+  }
+
+  def apply[A](aspect: String)(implicit statsdClient: StatsdClient, metric: Metric[A]): Sink[A] = {
+    MetricSink(statsdClient.withAspect(aspect), metric)
+  }
+
+  def apply[A](aspect: String, sampleRate: SampleRate)(implicit statsdClient: StatsdClient, metric: Metric[A]): Sink[A] = {
+    MetricSink(statsdClient.withAspect(aspect).sampledAt(sampleRate), metric)
+  }
 }
